2c19b16df746356b16c6ec20f1568e9f1bd051c6
Christian Kuri <christian.kuri.martinez@gmail.com>
2021-11-30 10:39:32 -0600

    truffle compile

--- contracts/BasicToken.sol
+++ contracts/BasicToken.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.10;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract BasicToken is ERC20 {
-    constructor() ERC20("Gold", "GLD") {
+    constructor() ERC20("BasicToken", "BTKN") {
         _mint(msg.sender, 1000);
     }
 }
