689542ed7cb56dbba7b39bcd4eea03c4768d05dc
Christian Kuri <christian.kuri.martinez@gmail.com>
2021-11-30 10:53:48 -0600

    Update token to have 1000 tokens and build again

--- contracts/BasicToken.sol
+++ contracts/BasicToken.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
+// Token Address: https://testnet.bscscan.com/token/0x99c22b783afa4ee01c7ac3ffb5f1a4fc8e7caa05
 pragma solidity ^0.8.10;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract BasicToken is ERC20 {
     constructor() ERC20("BasicToken", "BTKN") {
-        _mint(msg.sender, 1000);
+        _mint(msg.sender, 1000 * 10**18);
     }
 }
